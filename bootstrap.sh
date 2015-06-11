272a3002d4c11f4c6d04008d9a146376e7837ffb
Tim Anema <timanema@gmail.com>
2015-06-11 11:01:02 -0400

    added my vim setup so I dont need to install everything on a new system, also added a bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# quickly setup my env
+
+cp ./gitconfig ~/.gitconfig
+cp ./zshrc ~/.zshrc
+
+cp -r ./vim ~/.vim
+ln -s ~/.vim/vimrc ~/.vimrc
+
+cp -r ./tmux ~/.tmux
+ln -s ~/.tmux/tmux.conf ~/.tmux.conf
+
+cp -r ./irssi ~/.irssi
+
