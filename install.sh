ac217b7a57745a4eeffb3336ec439b0c26e28b33
Tim Anema <tim.anema@shopify.com>
2021-05-26 11:05:16 -0400

    simplify

--- install.sh
+++ install.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env bash
-echo "Linking config files"
 ln -sf ~/dotfiles/gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/zshrc ~/.zshrc
 ln -sf ~/dotfiles/vim ~/.vim
 ln -sf ~/dotfiles/tmux ~/.tmux
 ln -sf ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
-
-echo "installing vundle and vim plugins"
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-vim +BundleInstall +qall
-
-echo "Installing tmux plugins"
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-git clone https://github.com/tmux-plugins/tmux-yank ~/.tmux/plugins/tmux-yank
-git clone https://github.com/tmux-plugins/tmux-sensible ~/.tmux/plugins/tmux-sensible
