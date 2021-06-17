b184ec2ecf697631809f4c8870aac340e2bcd61b
Tim Anema <tim.anema@shopify.com>
2021-06-17 15:31:50 -0400

    just work already

--- install.sh
+++ install.sh
@@ -6,13 +6,12 @@ ln -sf ~/dotfiles/vim ~/.vim
 ln -sf ~/dotfiles/tmux ~/.tmux
 ln -sf ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
 
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 mkdir ~/.ssh
 echo "Host github.com
   StrictHostKeyChecking no" >> ~/.ssh/config
-
 git config commit.gpgsign false
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth 1 https://github.com/dracula/zsh.git ~/.oh-my-zsh/custom/theme/dracula
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
