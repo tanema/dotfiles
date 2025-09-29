0781949dfc5f08e883b9893ec0956f0d427a0fd2
Tim Anema <tim.anema@pocket.health>
2025-09-29 15:00:30 -0400

    Fix persistent error in my autocmd

--- config/nvim/plugin/autocmd.lua
+++ config/nvim/plugin/autocmd.lua
@@ -1,22 +1,22 @@
-vim.api.nvim_create_autocmd({"BufReadPost"}, {
-  desc = "Restore cursor position and refresh if any changes when loading a buffer",
-  callback = function()
-    local lastpos = vim.fn.line("'\"")
-    if lastpos > 0 or lastpos <= vim.fn.line("$") then
-      vim.cmd.normal("'\"")
-    end
-    vim.cmd.checktime();
-  end,
+vim.api.nvim_create_autocmd({ "BufReadPost" }, {
+	desc = "Auto jump to last position",
+	group = vim.api.nvim_create_augroup("auto-last-position", { clear = true }),
+	callback = function(args)
+		local position = vim.api.nvim_buf_get_mark(args.buf, [["]])
+		local winid = vim.fn.bufwinid(args.buf)
+		pcall(vim.api.nvim_win_set_cursor, winid, position)
+	end,
 })
 
-vim.api.nvim_create_autocmd({"BufWritePre"}, {
-  desc = "Fix style changes on save.",
-  callback = function()
-    -- fix tabs
-    vim.cmd.retab()
-    -- fix style
-    vim.cmd("silent Neoformat")
-    -- remove space at the end of each line
-    vim.cmd([[%s/\s\+$//e]])
-  end,
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+	desc = "Fix style changes on save.",
+	group = vim.api.nvim_create_augroup("style-fix-on-save", { clear = true }),
+	callback = function()
+		-- fix tabs
+		vim.cmd.retab()
+		-- fix style
+		vim.cmd("silent Neoformat")
+		-- remove space at the end of each line
+		vim.cmd([[%s/\s\+$//e]])
+	end,
 })
