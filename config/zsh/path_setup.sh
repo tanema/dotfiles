526a2cc3dc96e189184130b97ad5262d7e20ac2c
Tim Anema <tim.anema@dealmaker.tech>
2026-06-30 15:00:08 -0400

    Add lsps to claude

--- config/zsh/path_setup.sh
+++ config/zsh/path_setup.sh
@@ -1,13 +1,13 @@
-export PATH="" # reset so my path will work as expected without touching /etc/paths
-export PATH=$PATH:/opt/homebrew/bin:/opt/homebrew/sbin
-export PATH=$PATH:$GOPATH/bin
-export PATH=$PATH:$RBENV_ROOT/shims
-export PATH=$PATH:$HOME/bin
-export PATH=$PATH:$HOME/.local/bin
-export PATH=$PATH:$HOME/.cargo/bin
-export PATH=$PATH:/usr/local/bin
-export PATH=$PATH:/usr/local/sbin
-export PATH=$PATH:/usr/bin
-export PATH=$PATH:/usr/sbin
-export PATH=$PATH:/bin
-export PATH=$PATH:/sbin
+# This file is entirely devoted to setting up my $PATH variable to ensure a clean
+# and properly ordered $PATH in every environment without having to touch /etc/paths
+# which requires sudo. It also ensure that if anything has touched my /etc/paths
+# it will no get touched anyways.
+export PATH=""                                         # reset so my path to empty.
+export PATH=$PATH:/opt/homebrew/bin:/opt/homebrew/sbin # use homebrew over any system command
+export PATH=$PATH:$HOME/.local/bin                     # custom local system commands if I make them.
+export PATH=$PATH:$GOPATH/bin                          # golang tools
+export PATH=$PATH:$RBENV_ROOT/shims                    # rbenv shims
+export PATH=$PATH:$HOME/.cargo/bin                     # rust tools
+export PATH=$PATH:/usr/local/bin:/usr/local/sbin       # local takes precedence over /usr
+export PATH=$PATH:/usr/bin:/usr/sbin                   # usr takes precedence over /
+export PATH=$PATH:/bin:/sbin                           # root tools
