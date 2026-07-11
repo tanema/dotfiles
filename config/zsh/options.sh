bdfce8b541590df82ad5c2250921b962eba632c4
Tim Anema <timanema@gmail.com>
2026-07-11 13:58:25 -0400

    Fix compinit warning message

--- config/zsh/options.sh
+++ config/zsh/options.sh
@@ -1,5 +1,5 @@
 # Better history usage
-autoload -U compinit; compinit
+autoload -Uz compinit; compinit -u
 autoload -U history-search-end
 zle -N history-beginning-search-backward-end history-search-end
 zle -N history-beginning-search-forward-end history-search-end
